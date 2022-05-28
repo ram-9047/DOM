706ced66a0e2b6f1f8cadd9aa0d645734a58be0f
ram-9047 <ramg786@live.com>
2022-05-28 16:00:47 +0530

    completes TASK 5

--- index.js
+++ index.js
@@ -9,9 +9,21 @@
 
 // ---------------------------TASK 4--------------------
 
-let items = document.getElementsByClassName("list-group-item");
-// console.log(items[2]);
-items[2].style.backgroundColor = "green";
-for (let i = 0; i < items.length; i++) {
-  items[i].style.fontWeight = "bold";
+// let items = document.getElementsByClassName("list-group-item");
+// // console.log(items[2]);
+// items[2].style.backgroundColor = "green";
+// for (let i = 0; i < items.length; i++) {
+//   items[i].style.fontWeight = "bold";
+// }
+
+// --------------------------TASK 5 ---------------------------
+
+let li = document.getElementsByTagName("li");
+console.log(li);
+li[2].innerHTML = "Changed using Tag Name";
+li[2].style.fontSize = "1.2rem";
+li[2].style.color = "purple";
+
+for (let i = 0; i < li.length; i++) {
+  li[i].style.background = "#817D8B";
 }
