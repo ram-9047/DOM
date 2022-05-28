c1e4947d88f05a81fad3358dd32a6f0cb97d7674
ram-9047 <ramg786@live.com>
2022-05-28 14:11:59 +0530

    changes done

---
+++ index.js
@@ -0,0 +1,6 @@
+let headerTitle = document.getElementById("header-title");
+headerTitle.style.borderBottom = "solid 0.3rem black";
+
+let bodyTitle = document.getElementsByClassName("title");
+bodyTitle[0].style.fontWeight = "bold";
+bodyTitle[0].style.color = "green";
