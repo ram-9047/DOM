e4ddb1a311a75ce14c36343be535d2ffe0f59088
ram-9047 <ramg786@live.com>
2022-05-28 15:49:45 +0530

    changes in font of list-items and background of 3rd item

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
-let headerTitle = document.getElementById("header-title");
-headerTitle.style.borderBottom = "solid 0.3rem black";
+// ------------TASK 3---------------
 
-let bodyTitle = document.getElementsByClassName("title");
-bodyTitle[0].style.fontWeight = "bold";
-bodyTitle[0].style.color = "green";
+// let headerTitle = document.getElementById("header-title");
+// headerTitle.style.borderBottom = "solid 0.3rem black";
+
+// let bodyTitle = document.getElementsByClassName("title");
+// bodyTitle[0].style.fontWeight = "bold";
+// bodyTitle[0].style.color = "green";
+
+// ---------------------------TASK 4--------------------
+
+let items = document.getElementsByClassName("list-group-item");
+// console.log(items[2]);
+items[2].style.backgroundColor = "green";
+for (let i = 0; i < items.length; i++) {
+  items[i].style.fontWeight = "bold";
+}
